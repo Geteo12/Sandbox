085739d339034e3c1ec979822f38d9e9f3b6f5c7
Geteo12 <Geteolastpiece@gmail.com>
2022-01-17 11:33:02 +0100

    Ajout d'une methode configure avec les utilisateurs et cryptage du mdp

--- src/main/java/com/gto/sandbox/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/gto/sandbox/configuration/SpringSecurityConfiguration.java
@@ -1,14 +1,25 @@
 package com.gto.sandbox.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity; 
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	public void configure (AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user").password(passwordEncoder().encode("user")).roles("USER")
+		.and()
+		.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER");
+	}
 	@Override
 	public void configure(HttpSecurity http)throws Exception {
 		
@@ -19,5 +30,11 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin();
 	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder () {
+		return new BCryptPasswordEncoder();
+	}
+	
 	
 }
